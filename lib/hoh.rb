d3a60ed75ed84f0d3da607a35c923c3e94b5f8ee
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 22:01:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -33,11 +33,22 @@
 def naming_system
 
   {:label=>"Kingdom",
-   :sub_category=> {:label=>"Phylum",
-                    :sub_category=> {:label=>"Class",
-                                      :sub_category => PORTION_2}
-                                      }
-                                      }
+   :sub_category=> {
+     :label=>"Phylum",
+     :sub_category=> {
+       :label=>"Class",
+       :sub_category => {
+         :label => "Order",
+         :sub_category =>{
+           label: "Family",
+           sub_category: {
+             label: "Genus",
+             sub_category: {
+             label: "Species",
+             sub_category: nil
+           }
+         }
+} } } } }
                                       
   # Remember:
   #  Kingdom
