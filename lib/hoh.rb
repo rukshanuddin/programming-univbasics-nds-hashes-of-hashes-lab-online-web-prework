0e50b442fd805d7c9a586f994775342ec686ac05
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 21:37:27 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -34,7 +34,8 @@ def naming_system
 
   {:label=>"Kingdom",
    :sub_category=> {:label=>"Phylum",
-                    :sub_category=>{:label=>"Class", :sub_category => {PORTION_2 => {:sub_category => PORTION_3}}}}}
+                    :sub_category=> {:label=>"Class",
+                                      :sub_category => {PORTION_2[sub_category] => PORTION_3}}}}}
   # Remember:
   #  Kingdom
   #  Phylum
