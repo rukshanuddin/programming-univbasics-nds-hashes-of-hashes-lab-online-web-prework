53a5377b7237ee9539c4b3a55fd6c62937aa5000
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 22:05:00 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,7 +38,7 @@ def naming_system
      :sub_category=> {
        :label=>"Class",
        :sub_category => {
-         PORTION_2,
+         :label => "Order",
          :sub_category =>{
            label: "Family",
            sub_category: {
