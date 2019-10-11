e3d75e7c08ed8a46782ead4181631eb92b3430b3
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 20:44:42 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@
  }
 
 def naming_system
-  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] => PORTION_2
+  PORTION_1[:sub_category][:sub_category][:sub_category] = {sub_category: PORTION_2}
   # Remember:
   #  Kingdom
   #  Phylum
