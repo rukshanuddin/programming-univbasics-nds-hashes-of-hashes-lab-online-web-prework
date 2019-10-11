8bcc2b19c524af2256cab9872670488dce99d7be
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-11 21:14:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,8 @@
  }
 
 def naming_system
-  PORTION_1[:sub_category][:sub_category] << {sub_category: PORTION_2}
+  naming_system = PORTION_1
+  naming_system[:sub_category][:sub_category] << {:sub_category => PORTION_2} 
 #  {:label=>"Kingdom",
 #   :sub_category=> {:label=>"Phylum",
 #                    :sub_category=>{:label=>"Class", :sub_category => PORTION_2}}}
